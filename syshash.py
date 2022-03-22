06e02be7343e45c75ab22dce43e4c2c2c2ed013a
lbackerman <89616893+lbackerman@users.noreply.github.com>
2022-03-22 14:08:51 -0400

    Add files via upload

---
+++ syshash.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3 -B
+
+#Lucy Ackerman
+#m220024
+#Lab5 
+#Resources: (1) hashit3.py; this program is hashit3.py but with a few line changes for the STDOUT
+
+# Import the required libraries
+import os, sys, hashlib, time
+
+# Away to automatically abort unhandleable files...
+import signal
+
+class Alarm(Exception):
+    pass
+
+def alarm_handler(signum, frame):
+    raise Alarm
+
+# Get the root of the filesystem to scan from the command line
+if len(sys.argv) < 2:
+    print('Please provide the root of the directory structure to scan as an argument!')
+    sys.exit()
+BASEDIR = sys.argv[-1]
+
+# Ignorable Directories and Files
+IGNORE_DIR = ['/dev', '/proc', '/run', '/sys', '/tmp','/var/lib', '/var/run']
+IGNORE_FILE = ['../J-01.So Much Data/notes.txt']
+
+'''
+Retrieve stored metadata
+'''
+def load_metadata(filename='hashit.mtd'):
+    try:
+        results = open(filename,'r').readlines()
+        results = eval(results[0])
+        return results[0], results[1]
+    except:
+        return {}, {}
+
+'''
+Save metadata
+'''
+def save_metadata(FDB, HDB, filename='hashit.mtd'):
+    try:
+        results = open(filename, 'w')
+        results.write(str([FDB,HDB])+'\n')
+        results.close()
+    except:
+        pass
+
+'''
+Hash the files
+'''
+def hashit(filename):
+    BUFFER_SIZE = 65536
+    sha256 = hashlib.sha256()
+    try:
+        # Break the try/except after 2 seconds if the hash continues
+        # I am impatient...
+        signal.signal(signal.SIGALRM, alarm_handler)
+        signal.alarm(2)
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(BUFFER_SIZE)
+                if not data:
+                    break
+                sha256.update(data)
+        signal.alarm(0)
+    except IOError:
+        pass
+    except Alarm:
+        print('I\'m bored, moving on...')
+    return sha256.hexdigest()
+
+# Store the information in a way that we can work with it later.
+FDB, HDB = load_metadata()
+
+# Start the scan
+for root, directory, files in os.walk(BASEDIR):
+    PATH_OK = True
+    for check in IGNORE_DIR:
+        if check.find(root) == 0 or root.find(check) == 0:
+            PATH_OK = False
+    if PATH_OK: #Changes were made to this portion of hashit3.py in order to make it a useful tool for a user. The changes made below 
+        #are solely to the print statements in order to only show the user what the most recent modifications were in the file 
+        #system - nothing more 
+        print("Modification of File System Since Last Program Run:\n")
+        for file in files:
+            this_file = root + '/' + file
+            if this_file not in IGNORE_FILE:
+                this_hash = hashit(this_file)
+                this_time = time.time()
+                if this_file in FDB and FDB[this_file][0] != this_hash:
+                    print('MODIFIED:'),
+                    print(this_file)
+                    print(this_hash + "\n")
+                    print('LAST HASH TIME:'),
+                    print(time.asctime(time.localtime(FDB[this_file][1]))+ "\n")
+                FDB[this_file] = [this_hash, this_time]
+                HDB[this_hash] = [this_file, this_time]
+
+# Save the results
+save_metadata(FDB, HDB)
